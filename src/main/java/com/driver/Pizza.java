aa6a03e1c7badca80adb78ef741d2c9fddb1424d
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-18 02:56:43 +0530

    2nd commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -11,11 +11,11 @@ public class Pizza {
         // your code goes here
         if(isVeg==true){
             this.price = 300; 
-            this.bill += "Veg pizza base price = "+this.price +"\n";
+            this.bill += "Veg pizza base price: "+300 +"\n";
         }
         else{
             this.price = 400;
-            this.bill += "Non-veg pizza base price = "+this.price +"\n";
+            this.bill += "Non-veg pizza base price: "+400 +"\n";
         }
     }
 
@@ -26,29 +26,30 @@ public class Pizza {
     public void addExtraCheese(){
         // your code goes here
         this.price  = this.price + 80;
-        this.bill += "Extra Cheese Price = "+this.price +"\n";
+        this.bill += "Extra Cheese Price: "+80 +"\n";
     }
 
     public void addExtraToppings(){
         // your code goes here
         if(isVeg = true){
             this.price = this.price + 70; 
-            this.bill += "Extra Toppings For Veg Pizza = "+this.price +"\n";
+            this.bill += "Extra Toppings For Veg Pizza: "+70 +"\n";
         }
         else{
             this.price = this.price + 120;
-            this.bill += "Extra Toppings For Non-veg Pizza = "+this.price +"\n";
+            this.bill += "Extra Toppings For Non-veg Pizza: "+120 +"\n";
         }
     }
 
     public void addTakeaway(){
         // your code goes here
         this.price = this.price + 20; 
-        this.bill += "Paper bag Price = "+this.price;
+        this.bill += "Paper bag Price: "+20 +"\n";
     }
 
     public String getBill(){
         // your code goes here
+        this.bill += "Total Price: "+this.price;
         return this.bill;
     }
 }
