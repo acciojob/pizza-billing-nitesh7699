b216cbb04f56dc129ee446213482fbcb9915d862
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-22 23:42:35 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
 
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+    //System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
